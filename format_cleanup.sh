4bef3b3b8fbf5bc6118ee125d21e3fd7d2c867ed
riddhiman92 <riddhimankg@utexas.edu>
2017-09-15 18:51:00 -0500

    Clean_up scripts

---
+++ format_cleanup.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+############################################################
+# Combine counts from msblender pep_count files.
+# 
+#   Example usage
+#   combine_pepcounts.sh *.pep_count_mFDRpsm001 > outfile
+# 
+# 
+# These files have two header lines, followed by
+#   <peptide> <total_count> <sample_count>
+#
+# OUTPUT - Table of all peptides counts across all samples:
+#   <peptide> <total_count> <count_s1> <count_s2> ...
+# 
+# 
+#   Note: Each file is assumed to contain counts
+#         for only a single sample!!
+# 
+############################################################
+
+awk 'BEGIN {
+    FS="\t"
+    OFS=FS
+    PROCINFO["sorted_in"] = "@ind_str_asc"
+  }
+  
+  FNR == 1 {
+    filecounter++
+    split(FILENAME,splitted,"_")
+    if (filecounter == 1)
+      printf($2"\tTotalCount\tUniquePeptides")
+    printf("\t%s_%s",splitted[2],splitted[6])
+    next
+  }
+  #$2 ~ /\,/ && $24 /Confident/ {
+  $2 !~ /,/ {
+    pep_totals[$3] += 1
+    peps[$3,filecounter] += 1
+    prot[$3]=$2
+  }
+  END {
+    for (pep in pep_totals) {
+      printf("\n%s\t%d\t%s", pep, pep_totals[pep], prot[pep])
+      for (i = 1; i<=filecounter; i++)
+        printf("\t%d", peps[pep,i])
+    }
+  }' "$@"
+
+
+#FNR == 2 {
+#    if (filecounter == 1)
+#      printf($1"\tTotalCount")
+#    printf("\t%s",$3)
+#    next
+#  }
+#  {
+#    pep_totals[$1] += $3
+#    peps[$1,filecounter] = $3
+#  }
+#  END {
+#    for (pep in pep_totals) {
+#      printf("\n%s\t%d", pep, pep_totals[pep])
+#      for (i = 1; i<=filecounter; i++)
+#        printf("\t%d", peps[pep,i])
+#    }
+#  }' "$@"
